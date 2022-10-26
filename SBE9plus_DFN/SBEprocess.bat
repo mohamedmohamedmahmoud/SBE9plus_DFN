9782c9a4a6b7f06a3370b7ae51978a6124e77bde
mohamedmohamedmahmoud <116378505+mohamedmohamedmahmoud@users.noreply.github.com>
2022-10-26 09:48:19 +0200

    Add files via upload

---
+++ SBE9plus_DFN/SBEprocess.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32
+SBEBatch.exe  C:\Users\a33272\Desktop\sbe_psa\SBE9plus_DFN\SBEprocess.txt  C:\Users\a33272\Desktop\sbe_psa\SBE9plus_DFN\DATA_Files
