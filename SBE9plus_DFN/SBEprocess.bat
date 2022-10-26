0ed3826240dabd5a27aa7649f3b4540f1d1465c4
mohamedmohamedmahmoud <116378505+mohamedmohamedmahmoud@users.noreply.github.com>
2022-10-26 09:50:23 +0200

    Delete SBE9plus_DFN directory

--- SBE9plus_DFN/SBEprocess.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Program Files (x86)\Sea-Bird\SBEDataProcessing-Win32
-SBEBatch.exe  C:\Users\a33272\Desktop\sbe_psa\SBE9plus_DFN\SBEprocess.txt  C:\Users\a33272\Desktop\sbe_psa\SBE9plus_DFN\DATA_Files
